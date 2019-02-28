752b04992999729406264b45954b7fb5954973ca
John Kawahara <kawaharajohn@gmail.com>
2019-02-28 09:03:53 -0800

    Updates

---
+++ precache-manifest.cf249a5770d3a25ef83a69256bfa682e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "624ea07b209952092620",
+    "url": "/static/js/main.624ea07b.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "624ea07b209952092620",
+    "url": "/static/css/main.d89dc61b.chunk.css"
+  },
+  {
+    "revision": "0d5bd1b88adf899fb9cd7053442948e4",
+    "url": "/index.html"
+  }
+];
