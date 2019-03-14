e3e96e95c7916d2a322c99f4156f2e31fe7dc41d
John Kawahara <kawaharajohn@gmail.com>
2019-03-14 15:57:09 -0700

    Plan RSVP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
+// Single Page Application
 import React, { Component } from 'react';
 import Nav from "./components/Nav";
-// import Splash from "./components/Splash";
+import Timing from "./components/Timing";
+import Splash from "./components/Splash";
 // import Story from "./components/Story";
-// import Timing from "./components/Timing";
 // import Events from "./components/Events";
 // import Gallery from "./components/Gallery";
 // import Registry from "./components/Registry";
-// import Rsvp from "./components/Rsvp";
-import splashpic from "./splashpic.jpeg";
+import Rsvp from "./components/Rsvp";
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Nav />
-        {/* <Splash />
-        <Story />
-        <Timing />
-        <Events />
-        <Gallery />
-        <Registry />
-        <Rsvp /> */}
-      </div>
+      <Container className="mainapp">
+        <Row>
+          <Col>
+            <Nav />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Timing />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Splash />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Rsvp />
+          </Col>
+        </Row>
+        {/* <Story /> */}
+        {/* <Events /> */}
+        {/* <Gallery /> */}
+        {/* <Registry /> */}
+      </Container>
     );
   }
 }
---
+++ src/components/Rsvp/index.js
@@ -0,0 +1,58 @@
+// *** RSVP component
+import React, { Component } from 'react';
+import "./style.css";
+import { Container, Row, Col, Form, Button} from "react-bootstrap";
+
+class Rsvp extends Component {
+  render() {
+    return (
+      <Container id="rsvp">
+        <Row>
+          <Col>RSVP Response By The Eleventh Of April</Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form action="https://formspree.io/jjshon@gmail.com" method="POST">
+              <Row>
+                <Col sm={{ span: 4, offset: 1 }}>
+                  <Form.Control className="forminput border border-secondary" type="string" placeholder="First & Last Name" name="Name" required />
+                  <Form.Control className="forminput border border-secondary" type="number" placeholder="Number Attending" name="Number Attending" />        
+                </Col>
+                <Col sm={4}>
+                  <Form.Group as={Row}>
+                    <Col sm={10}>
+                      <Form.Check className="formradio"
+                        type="radio"
+                        label="Accepts With Pleasure"
+                        name="Attendance at Christine & Salvador's Marriage"
+                        value="Accepts With Pleasure"
+                        id="formHorizontalRadios1"
+                        required
+                      />
+                      <Form.Check
+                        type="radio"
+                        label="Declines With Regret"
+                        name="Attendance at Christine & Salvador's Marriage"
+                        value="Declines With Regret"
+                        id="formHorizontalRadios2"
+                        required
+                      />
+                    </Col>
+                  </Form.Group>
+                </Col>
+                <Col sm={2}>
+                  <Button variant="outline-secondary" type="submit">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+// Export function for importing into /pages
+export default Rsvp;
