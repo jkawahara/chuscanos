2e95f48310b3a053e245b86a11af36ac918c5053
John Kawahara <kawaharajohn@gmail.com>
2019-03-17 20:47:28 -0700

    Fix RSVP form spacing

--- src/components/Rsvp/index.js
+++ src/components/Rsvp/index.js
@@ -14,33 +14,49 @@ class Rsvp extends Component {
           <Col>
             <Form action="https://formspree.io/jjshon@gmail.com" method="POST">
               <Row className="forminputs">
-                <Col sm={{ span: 4, offset: 1 }}>
-                  <Form.Control className="forminput border border-secondary" type="string" placeholder="First & Last Name" name="Name" required />
-                  <Form.Control className="forminput border border-secondary" type="number" placeholder="Number Attending" name="Number Attending" />        
+                <Col sm={{ span: 6, offset: 3 }}>
+                  <p>
+                    <Form.Control className="forminput border border-secondary" type="string" placeholder="First & Last Name" name="Name" required />
+                  </p>
                 </Col>
-                <Col sm={4}>
+              </Row>
+              <Row> 
+                <Col sm={{ span: 6, offset: 3 }}>
+                  <p>
+                    <Form.Control className="forminput border border-secondary" type="number" placeholder="Number Attending" name="Number Attending" />
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col sm={{ span: 6, offset: 3 }}>
                   <Form.Group as={Row}>
-                    <Col sm={10}>
-                      <Form.Check className="formradio"
-                        type="radio"
-                        label="Accepts With Pleasure"
-                        name="Attendance at Christine & Salvador's Marriage"
-                        value="Accepts With Pleasure"
-                        id="formHorizontalRadios1"
-                        required
-                      />
-                      <Form.Check
-                        type="radio"
-                        label="Declines With Regret"
-                        name="Attendance at Christine & Salvador's Marriage"
-                        value="Declines With Regret"
-                        id="formHorizontalRadios2"
-                        required
-                      />
+                    <Col>
+                      <p>
+                        <Form.Check className="formradio"
+                          type="radio"
+                          label="Accepts With Pleasure"
+                          name="Attendance at Christine & Salvador's Marriage"
+                          value="Accepts With Pleasure"
+                          id="formHorizontalRadios1"
+                          required
+                        />
+                      </p>
+                      <p>
+                        <Form.Check
+                          type="radio"
+                          label="Declines With Regret"
+                          name="Attendance at Christine & Salvador's Marriage"
+                          value="Declines With Regret"
+                          id="formHorizontalRadios2"
+                          required
+                        />
+                      </p>
                     </Col>
                   </Form.Group>
                 </Col>
-                <Col sm={2}>
+              </Row>
+              <Row>
+                <Col>
                   <Button variant="primary" type="submit">
                     Submit
                   </Button>
