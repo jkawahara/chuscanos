5d00c738cfff8ea58d9de80b9cebbcafc07dd31b
John Kawahara <kawaharajohn@gmail.com>
2019-03-17 21:52:18 -0700

    Add contact fields

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 // *** Story component
 import React, { Component } from 'react';
 import "./style.css";
-import { Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 class Footer extends Component {
   render() {
@@ -13,7 +13,41 @@ class Footer extends Component {
           </Col>
         </Row>
         <Row>
-          <Col>More to come..</Col>
+          <Col>
+            <Form action="https://formspree.io/cjchu@gene.com" method="POST">
+              <Row className="forminputs">
+                <Col sm={{ span: 6, offset: 3 }}>
+                  <p>
+                    <Form.Control className="forminput border border-secondary" type="string" placeholder="First & Last Name" name="Name" required />
+                  </p>
+                </Col>
+              </Row>
+              <Row> 
+                <Col sm={{ span: 6, offset: 3 }}>
+                  <p>
+                    <Form.Control className="forminput border border-secondary" type="email" placeholder="example@gmail.com" name="Email" required />
+                  </p>
+                </Col>
+              </Row>
+              <Row> 
+                <Col sm={{ span: 6, offset: 3 }}>
+                  <p>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                      {/* <Form.Label>Message...</Form.Label> */}
+                      <Form.Control as="textarea" rows="6" className="forminput border border-secondary" name="Message" placeholder="Message..." required />
+                    </Form.Group>
+                  </p>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
         </Row>
       </Container>
     );
