484e613fe46be9f0240cf2f7850c1798e8321868
John Kawahara <kawaharajohn@gmail.com>
2019-03-19 16:17:15 -0700

    Add image upload

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,30 +1,77 @@
 // *** Story component
 import React, { Component } from 'react';
 import "./style.css";
-import { Container, Row, Col, Carousel } from "react-bootstrap";
+import { Container, Row, Col, Carousel, Form } from "react-bootstrap";
+import firebase from "firebase/app";
+import "firebase/storage";
+import FileUploader from "react-firebase-file-uploader";
 
 class Gallery extends Component {
+  state = {
+    avatar: "",
+    isUploading: false,
+    progress: 0,
+    avatarURL: ""
+  };
+ 
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+
+  handleProgress = progress => this.setState({ progress });
+  
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
+
   render() {
     return (
       <Container id="gallery">
         <Row>
           <Col id="titlegallery">Gallery</Col>
         </Row>
-        {/* <Row>
-          <Col id="titlefile">Send us a pic of Christine & Sal...</Col>
-        </Row>
-        <Row id="filesubmit">
-          <Col sm={{ span: 4, offset: 4 }}>
-            <Form.Control className="forminput border border-secondary file-select" type="file" name="File" accept="image/*" required />
-          </Col>
+        <Row>
+          <Col id="titlefile">Send us a pic of Christine & Salvador (10 MB max image size)</Col>
         </Row>
         <Row>
           <Col>
-            <Button className="file-submit" variant="primary" type="submit">
-              Submit
-            </Button>
+            <Form>
+              <Row>
+                <Col>
+                  {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+                </Col>
+              </Row>
+                <Col>
+                  {this.state.avatarURL && <img src={this.state.avatarURL} id="uploadedimg" alt="upload" />}
+                </Col>
+              <Row>
+                <Col sm={{ span: 6, offset: 3 }}>
+                  <FileUploader
+                    className="forminput border border-secondary"
+                    id="fileselect"
+                    accept="image/*"
+                    name="avatar"
+                    randomizeFilename
+                    storageRef={firebase.storage().ref("images")}
+                    onUploadStart={this.handleUploadStart}
+                    onUploadError={this.handleUploadError}
+                    onUploadSuccess={this.handleUploadSuccess}
+                    onProgress={this.handleProgress}
+                  />
+                </Col>
+              </Row>
+            </Form>
           </Col>
-        </Row> */}
+        </Row>
         <Row>
           <Col sm={{ span: 8, offset: 2 }}>
             <Carousel id="carousel">
