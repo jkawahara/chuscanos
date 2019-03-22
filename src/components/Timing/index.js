05b6595c2e48536a751077de9e8256c259f149d8
John Kawahara <kawaharajohn@gmail.com>
2019-03-22 16:01:39 -0700

    Simplify Timing component

--- src/components/Timing/index.js
+++ src/components/Timing/index.js
@@ -2,27 +2,14 @@
 import React, { Component } from 'react';
 import "./style.css";
 import { Container, Row, Col } from "react-bootstrap";
-import ReactMomentCountDown from 'react-moment-countdown';
 import moment from 'moment';
 
 class Timing extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { endCountdown: false }
-    this.sampleOnTick = this.sampleOnTick.bind(this)
-    this.sampleOnCountdownEnd = this.sampleOnCountdownEnd.bind(this)
-  }
-
-  sampleOnTick (countdown) {
-    this.setState({ endCountdown: countdown <= 0 })
-  }
-  sampleOnCountdownEnd () {
-    console.log('Ceremony Time!')
-  }
+  state = {
+    remainTime: moment().to('2019-05-26 15:30')
+  };
 
   render() {
-    const dateInFuture = moment('2019-05-26 15:30', 'YYYY-MM-DD HH:mm');
-
     return (
       <Container id="timing">
         <Row>
@@ -36,37 +23,8 @@ class Timing extends Component {
           </Col>
         </Row>
         <Row>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col sm={{ span: 2, offset: 5 }}>
-            <ReactMomentCountDown toDate={dateInFuture}
-              targetFormatMask='WW'
-              onTick={this.sampleOnTick}
-              onCountdownEnd={this.sampleOnCountdownEnd}
-            /><br></br>
-            <p>Weeks</p>
-          </Col> 
-          {/* <Col sm={{ span: 2, offset: 4 }}>
-            <ReactMomentCountDown toDate={dateInFuture}
-              targetFormatMask='DD'
-              onTick={this.sampleOnTick}
-              onCountdownEnd={this.sampleOnCountdownEnd}
-            /><br></br>
-            <p>Days</p>
-          </Col> 
-          <Col sm={2}>
-            <ReactMomentCountDown toDate={dateInFuture}
-              targetFormatMask='HH'
-              onTick={this.sampleOnTick}
-              onCountdownEnd={this.sampleOnCountdownEnd}
-            /><br></br>
-            <p>Hours</p>
-          </Col> */}
-        </Row>
-        <Row>
-          <Col>
-            To Go
+          <Col id="titleremain" sm={{ span: 4, offset: 4 }}>
+            <p>{this.state.remainTime}</p>
           </Col>
         </Row>
       </Container>
